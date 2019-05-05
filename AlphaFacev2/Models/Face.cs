b083a4542fe71eb4f6e18a379cbf9f90dd679ce2
NicuVD <45542406+NicuVD@users.noreply.github.com>
2019-05-05 21:15:07 +0300

    Added Face comparison history

--- AlphaFacev2/Models/Face.cs
+++ AlphaFacev2/Models/Face.cs
@@ -4,8 +4,13 @@
     {
         public int Id { get; set; }
         public int ProfileId { get; set; }
-        public double Accuracy { get; set; }
+        public byte[] ProfileImage { get; set; }
+        public byte[] ComparisonImage { get; set; }
+        public bool IsIdentical { get; set; }
+        public double Confidence { get; set; }
         public string Gender { get; set; }
         public string FaceGuid { get; set; }
+        //public bool IsComparisonSucces { get; set; }
+
     }
 }
